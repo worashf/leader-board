0bf08682d88940650ca7a768f29df0ef380ba56d
worashf <uppert83@gmail.com>
2022-06-17 00:17:39 +0300

    add new score,clear and refresh feature

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import './asset/style.css';
-import Score from './Score';
-import appendAllScores from './Render';
+
+import { refreshPage, addNewScore } from './Render';
+
+import {
+  scoreFrom, userInput, scoreInput, refreshBtn,
+} from './Selector';
 
 window.onload = () => {
-  appendAllScores(Score);
+  refreshPage();
 };
+const clearInputs = () => {
+  userInput.value = '';
+  scoreInput.value = '';
+};
+
+scoreFrom.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const user = userInput.value;
+  const score = scoreInput.value;
+
+  addNewScore(user, score);
+  clearInputs();
+});
+refreshBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  refreshPage();
+});
